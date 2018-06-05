73b7663a13f0b7181db07fee718edf0c64594d10
Nadesh <nadesh@arkinfotec.com>
2018-06-05 16:36:35 +0530

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome Heroku Node Express' });
+  res.render('index', { title: 'Welcome Heroku Node Express App' });
 });
 
 module.exports = router;
